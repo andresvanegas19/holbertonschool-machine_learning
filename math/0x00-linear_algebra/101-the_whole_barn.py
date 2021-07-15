e3ba17de1416a98017dbccd2a5b4186b10e392cf
andres reyes <andressantiagore@gmail.com>
2021-07-15 06:23:25 -0500

    valdiate the dimensions of the matrix

--- math/0x00-linear_algebra/101-the_whole_barn.py
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -3,9 +3,50 @@
 That will add two matrices """
 
 
+def validate_matrix(matrix_one) -> bool:
+    """[summary]
+
+    Args:
+        matrix_one ([type]): [description]
+
+    Returns:
+        bool: [description]
+    """
+    #     if isinstance(type(matrix_one[0]), list):
+    #         return False
+
+    #     if len(matrix_one) != len(matrix_two):
+    #         return False
+
+    #     return True
+
+    i = len(matrix_one)
+    matrix_dimensions = [i]
+
+    while type(matrix_one[0]) == list:
+        matrix_one = matrix_one[0]
+        i = len(matrix_one)
+        matrix_dimensions.append(i)
+
+    return matrix_dimensions
+
+
+# TODO: Modificated the documentation
 def add_matrices(mat1, mat2):
-    """ that adds two matrices """
+    """[summary]
+
+    Args:
+        mat1 ([type]): [description]
+        mat2 ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
     new_m = []
+
+    if validate_matrix(mat1) != validate_matrix(mat2):
+        return None
+
     if isinstance(mat1[0], type(1)) and len(mat1) == len(mat2):
         for i in range(len(mat1)):
             new_m += [mat1[i] + mat2[i]]
@@ -19,4 +60,5 @@ def add_matrices(mat1, mat2):
         for i in range(len(matrix1)):
             suma += [matrix1[i] + matrix2[i]]
         new_m += [suma]
+
     return new_m
