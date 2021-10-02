2d5f7e69e1339e2e9deecb4f2399112b5abf1850
andres santiago reyes <andresdev@MacBook-Pro.local>
2021-10-02 01:44:27 -0500

    change the indentation of the function

--- supervised_learning/0x06-keras/8-train.py
+++ supervised_learning/0x06-keras/8-train.py
@@ -3,10 +3,10 @@
 import tensorflow.keras as Keras
 
 
-def train_model(network, data, labels, batch_size, epochs, validation_data=None,
-                early_stopping=False, patience=0, learning_rate_decay=False,
-                alpha=0.1, decay_rate=1, save_best=False, filepath=None,
-                verbose=True, shuffle=False
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1, decay_rate=1,
+                save_best=False, filepath=None, verbose=True, shuffle=False
                 ):
     """
     trains a model using mini-batch gradient descent:
