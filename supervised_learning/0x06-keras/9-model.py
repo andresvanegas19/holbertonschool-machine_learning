f95d331447bb0f47448ae2d0e417670070724927
andres santiago reyes <andresdev@MacBook-Pro.local>
2021-10-02 01:48:10 -0500

    replace function nonsense add the save ml

--- supervised_learning/0x06-keras/9-model.py
+++ supervised_learning/0x06-keras/9-model.py
@@ -4,72 +4,28 @@
 import tensorflow.keras as Keras
 
 
-def train_model(
-    network, data, labels, batch_size, epochs,
-    validation_data=None, early_stopping=False, patience=0,
-    learning_rate_decay=False, alpha=0.1, decay_rate=1,
-    save_best=False, filepath=None, verbose=True, shuffle=False
-):
+
+def save_model(network, filename):
+    """[summary]
+
+    Args:
+        network ([type]): [description]
+        filename ([type]): [description]
+
+    Returns:
+        [type]: [description]
     """
-    trains a model using mini-batch gradient descent:
+    network.save(filename)
+    return None
 
-    network is the model to train
-    data is a numpy.ndarray of shape (m, nx) containing the input data
-    labels is a one-hot numpy.ndarray of shape
-    (m, classes) containing the labels of data
 
-    batch_size is the size of the batch used for mini-batch gradient descent
-    epochs is the number of passes through data for
-    mini-batch gradient descent
+def load_model(filename):
+    """[summary]
 
-    verbose is a boolean that determines if output
-    should be printed during training
+    Args:
+        filename ([type]): [description]
 
-    shuffle is a boolean that determines whether to shuffle the
-    batches every epoch. Normally, it is a good idea to shuffle,
-    but for reproducibility, we have chosen to set the default to False.
-    early_stopping is a boolean that
-    indicates whether early stopping should be used
-        - early stopping should only be performed if validation_data exists
-        - early stopping should be based on validation loss
-    patience is the patience used for early stopping
-
-    Returns: the History object generated after training the model
+    Returns:
+        [type]: [description]
     """
-    def learning_rate_decay(epoch):
-        """ callback that calculate the epic of the rate of a range"""
-        alpha_utd = alpha / (1 + (decay_rate * epoch))
-        return alpha_utd
-
-    x = []
-
-    if early_stopping is True:
-        x.append(
-            Keras.callbacks.EarlyStopping(
-                monitor='val_loss',
-                patience=patience
-            )
-        )
-
-    if learning_rate_decay and validation_data:
-        x.append(
-            Keras.callbacks.LearningRateScheduler(
-                learning_rate_decay,
-                verbose=1
-            )
-        )
-
-    if save_best and validation_data:
-        x.append(
-            Keras.callbacks.ModelCheckpoint(filepath, save_best_only=True)
-        )
-
-    return network.fit(
-        x=data, y=labels,
-        callbacks=x,
-        epochs=epochs,
-        batch_size=batch_size,
-        validation_data=validation_data,
-        verbose=verbose,
-        shuffle=shuffle
-    )
+    return Keras.models.load_model(filename)
