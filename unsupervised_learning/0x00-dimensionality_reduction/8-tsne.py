31318cdfdcbf50ec0f981df8c4aeac3fb25b2e41
andres santiago reyes <andresdev@MacBook-Pro.local>
2021-10-27 22:35:08 -0500

    adding a new labell change the formula more about of the doc

--- unsupervised_learning/0x00-dimensionality_reduction/8-tsne.py
+++ unsupervised_learning/0x00-dimensionality_reduction/8-tsne.py
@@ -26,28 +26,43 @@ def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
     P = P_affinities(X, perplexity=perplexity) * 4
     # Y is a numpy.ndarray of shape (n, ndims) containing the embedded points
     Y = np.random.randn(n, ndims)
-    # iY is a numpy.ndarray of shape (n, ndims) containing the embedded points
+    # emc is a numpy.ndarray of shape (n, ndims) containing the embedded points
     emc = np.zeros((n, ndims))
+
+    # Perform early exaggeration
+    # gs is a numpy.ndarray of shape (n, ndims) and contains the early exaggeration
+    gs = np.ones((n, ndims))
+    momentum = 0.5
+    mg = 0.01
 
     for i in range(iterations):
         # Perform early exaggeration
         dY, Q = grads(Y, P)
 
         # momentum is the learning rate for the first 20 iterations
-        if i <= 20:
-            momentum = 0.5
-        else:
+        if i is 20:
             momentum = 0.8
+        if i is 100:
+            # delete the dot for avoiding errors
+            P = P / 4
 
-        # Update the embedding
-        Y = Y + (momentum * emc - lr * dY) - np.tile(np.mean(Y, 0), (n, 1))
+        # # Update the embedding
+        # Y = Y + (momentum * emc - lr * dY) - np.tile(np.mean(Y, 0), (n, 1))
+
+        if (i + 1) % 100 is 0:
+            print("Cost at iteration {}: {}".format(i + 1, cost(P, Q)))
 
         # Update the embedding for the next iteration
-        if (i + 1) != 0 and (i + 1) % 100 == 0:
-            C = cost(P, Q)
-            print("Cost at iteration {}: {}".format(i + 1, C))
+        gs = (gs + 0.2) * \
+            ((dY > 0.) != (emc > 0.)) + \
+            (gs * 0.8) * ((dY > 0.) == (emc > 0.))
 
-        if (i + 1) == 100:
-            P = P / 4.
+        # Update the embedding
+        gs[gs < mg] = mg
+        emc = momentum * emc - lr * (gs * dY)
+        Y = + emc
+        Y = - np.tile(np.mean(Y, 0), (n, 1))
+        # if (i + 1) == 100:
+        #     P = P / 4.
 
     return Y
