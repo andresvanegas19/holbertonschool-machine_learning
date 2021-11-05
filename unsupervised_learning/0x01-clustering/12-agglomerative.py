91853572cc9159884e57645a0f90d25db5ba017f
andres santiago reyes <andresdev@MacBook-Pro.local>
2021-11-04 19:15:59 -0500

    add the other method gmm

---
+++ unsupervised_learning/0x01-clustering/12-agglomerative.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Do clustering scikit-learn """
+
+import scipy.cluster.hierarchy
+import matplotlib.pyplot as plt
+
+
+def agglomerative(X, dist):
+    """
+    this function performs agglomerative clustering
+
+    Args:
+        X ([type]): [description]
+        dist ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+
+    Z = scipy.cluster.hierarchy.linkage(
+        X,
+
+        method='ward'
+    )
+
+    _ = plt.figure(figsize=(25, 10))
+    _ = scipy.cluster.hierarchy.dendrogram(
+        Z,
+        color_threshold=dist
+    )
+    plt.show()
+
+    return scipy.cluster.hierarchy.fcluster(
+        Z,
+        t=dist,
+        criterion='distance'
+    )
