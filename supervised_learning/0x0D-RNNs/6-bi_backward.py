132e3ca0b378c118037fb2aaf8c25f159d67f505
andres santiago reyes <andresdev@MacBook-Pro.local>
2021-12-04 02:20:41 -0500

    add more function to the class

---
+++ supervised_learning/0x0D-RNNs/6-bi_backward.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+""" RNNs """
+import numpy as np
+
+
+class BidirectionalCell():
+    """ Represents bidirectional cell of an RNN """
+
+    def __init__(self, i, h, o):
+        """ Class constructor for Bidirectional Cell """
+        self.Whf = np.random.normal(size=(i + h, h))
+        self.Whb = np.random.normal(size=(i + h, h))
+        self.Wy = np.random.normal(size=((2 * h), o))
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """
+        calculates the hidden state in the forward direction for one time step
+
+        Args:
+            x_t: contains the data input for the cell
+                - m is the batch size for the data
+            h_prev: containing the previous hidden state
+
+        Returns:
+            the next hidden state
+        """
+        return np.tanh(
+            np.concatenate(
+                (h_prev, x_t),
+                axis=1
+            ) @ self.Whf + self.bhf
+        )
+
+    def backward(self, h_next, x_t):
+        """
+        calculates the hidden state in the backward direction for one time step
+
+        Args:
+            x_t: contains the data input for the cell
+                - m is the batch size for the data
+            h_next: containing the next hidden state
+
+        Returns:
+            the previous hidden state
+        """
+        return np.tanh(
+            np.concatenate(
+                (h_next, x_t),
+                axis=1
+            ) @ self.Whb + self.bhb
+        )
