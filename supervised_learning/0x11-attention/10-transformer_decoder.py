91d2b238ac539b0421e1e6bea10372e13a11a3ca
andres santiago reyes <andresdev@MacBook-Pro.local>
2021-12-25 12:12:11 -0500

    chanign the operation and the constructor

--- supervised_learning/0x11-attention/10-transformer_decoder.py
+++ supervised_learning/0x11-attention/10-transformer_decoder.py
@@ -12,15 +12,16 @@ class Decoder(tf.keras.layers.Layer):
     def __init__(self, N, dm, h, hidden, target_vocab,
                  max_seq_len, drop_rate=0.1):
         """ init the decoder """
-        super(Decoder, self).__init__()
+        # not put the cass into the super
+        super().__init__()
         self.N = N
         self.dm = dm
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
         self.embedding = tf.keras.layers.Embedding(target_vocab, dm)
         self.positional_encoding = positional_encoding(max_seq_len, dm)
         self.blocks = [
             DecoderBlock(dm, h, hidden, drop_rate) for _ in range(N)
         ]
-        self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, encoder_output, training,
              look_ahead_mask, padding_mask):
@@ -37,15 +38,13 @@ class Decoder(tf.keras.layers.Layer):
         Returns: a tensor of shape (batch, target_seq_len, dm) containing the
             decoder output
         """
-        seq_len = x.shape[1]
-
-        # adding embedding and position encoding.
         x = self.embedding(x)
-        x = x * tf.math.sqrt(tf.cast(self.dm, tf.float32))
-        # x += self.positional_encoding[:seq_len]
-        x = x + self.positional_encoding[:seq_len]
+        x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        x += self.positional_encoding[:x.shape[1], :]
+        # mask the first multi head attention layer
         x = self.dropout(x, training=training)
 
+        # (batch_size, target_seq_len, d_model)
         for i in range(self.N):
             x = self.blocks[i](
                 x,
