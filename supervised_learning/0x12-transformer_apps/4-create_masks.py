0e5efb17470cc6c01583a06495065c1c5d993105
andres reyes <andressantiagore@gmail.com>
2022-01-07 18:17:15 -0500

    finish the mask

--- supervised_learning/0x12-transformer_apps/4-create_masks.py
+++ supervised_learning/0x12-transformer_apps/4-create_masks.py
@@ -1,2 +1,44 @@
 #!/usr/bin/env python3
 """ Transformer Applications """
+
+import tensorflow.compat.v2 as tf
+# tf.data.experimental.enable.debug_mode()
+
+def create_masks(inputs, target):
+    """
+    creates all masks for training/validation
+
+    Args:
+        inputs: is a tf.Tensor of shape (batch_size, seq_len_in)
+            that contains the input sentence
+        target: is a tf.Tensor of shape (batch_size, seq_len_out)
+            that contains the target sentence
+
+    Returns:
+        encoder_mask is the tf.Tensor padding mask of shape
+            (batch_size, 1, 1, seq_len_in) to be applied in the encoder
+        combined_mask is the tf.Tensor of shape
+            (batch_size, 1, seq_len_out, seq_len_out) used in the 1st
+            attention block in the decoder to pad and mask future tokens
+            in the input received by the decoder. It takes the maximum
+            between a look ahead mask and the decoder target padding mask.
+        decoder_mask is the tf.Tensor padding mask of shape
+            (batch_size, 1, 1, seq_len_in) used in the 2nd attention
+            block in the decoder.
+    """
+    inputs = tf.cast(tf.math.equal(inputs, 0), tf.float32)
+    target = tf.cast(tf.math.equal(target, 0), tf.float32)
+
+    encoder_mask = inputs[:, tf.newaxis, tf.newaxis, :]
+    decoder_mask = inputs[:, tf.newaxis, tf.newaxis, :]
+
+    dec_target_mask = \
+        target[:, tf.newaxis, tf.newaxis, :]  # mask future tokens
+    x, y = target.shape
+    look_ahead_mask = 1 - tf.linalg.band_part(
+        tf.ones((x, 1, y, y)), -1, 0
+    )
+
+    combined_mask = tf.maximum(dec_target_mask, look_ahead_mask)
+
+    return encoder_mask, combined_mask, decoder_mask
--- supervised_learning/0x12-transformer_apps/5-train.py
+++ supervised_learning/0x12-transformer_apps/5-train.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 """ Transformer Applications """
+
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+
+Dataset = __import__('3-dataset').Dataset
+create_masks = __import__('4-create_masks').create_masks
+Transformer = __import__('5-transformer').Transformer
+
+
+def train_transformer(N, dm, h, hidden, max_len, batch_size, epochs):
+    """"""
+    pass
