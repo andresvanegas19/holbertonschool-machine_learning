dc06934f8a94de9f04fba300013b33529ba28e66
“andresvanegas19” <andressantiagore@gmail.com>
2022-03-01 20:44:15 -0500

    adding files

--- pipeline/0x00-pandas/6-flip_switch.py
+++ pipeline/0x00-pandas/6-flip_switch.py
@@ -2,4 +2,10 @@
 """ pandas """
 
 import pandas as pd
-import numpy as np
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+df = df.T.iloc[:, ::-1]
+
+print(df.tail(8))
