bb01dc5178f06db3e0b5d82ccb5f39bdd65898b7
“andresvanegas19” <andressantiagore@gmail.com>
2022-03-01 20:23:20 -0500

    adding files

---
+++ s.sh
@@ -0,0 +1,19 @@
+
+
+touch pipeline/0x00-pandas/0-from_numpy.py
+touch pipeline/0x00-pandas/1-from_dictionary.py
+touch pipeline/0x00-pandas/10-index.py
+touch pipeline/0x00-pandas/11-concat.py
+touch pipeline/0x00-pandas/12-hierarchy.py
+touch pipeline/0x00-pandas/13-analyze.py
+touch pipeline/0x00-pandas/14-visualize.py
+touch pipeline/0x00-pandas/2-from_file.py
+touch pipeline/0x00-pandas/3-rename.py
+touch pipeline/0x00-pandas/4-array.py
+touch pipeline/0x00-pandas/5-slice.py
+touch pipeline/0x00-pandas/6-flip_switch.py
+touch pipeline/0x00-pandas/7-high.py
+touch pipeline/0x00-pandas/8-prune.py
+touch pipeline/0x00-pandas/9-fill.py
+touch pipeline/0x00-pandas/README.md
+touch pipeline/README.md
