3ddcd029834b67b5db82be38f787774bbe524037
“andresvanegas19” <andressantiagore@gmail.com>
2022-03-02 08:43:37 -0500

    finish the cleaning and modifing the data

--- pipeline/0x00-pandas/10-index.py
+++ pipeline/0x00-pandas/10-index.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 """ pandas """
 
-import pandas as pd
-import numpy as np
+from_file = __import__('2-from_file').from_file
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+df = df.set_index('Timestamp')
+
+print(df.tail())
