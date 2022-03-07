d3d47187114ea48bf6a10754990468b3c34c89cb
“andresvanegas19” <andressantiagore@gmail.com>
2022-03-07 09:56:32 -0500

    change the importing request

--- pipeline/0x01-apis/0-passengers.py
+++ pipeline/0x01-apis/0-passengers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ data collection from APIs """
-from requests import get
+import requests
 
 
 def availableShips(passengerCount):
@@ -20,7 +20,7 @@ def availableShips(passengerCount):
     state = True
 
     while state:
-        request = get(
+        request = requests.get(
             f"https://swapi-api.hbtn.io/api/starships/?page={page}"
         )
         data = request.json()
