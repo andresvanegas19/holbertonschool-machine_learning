6515c76b166ceba413e28c5429ab4b8e9817c8b4
“andresvanegas19” <andressantiagore@gmail.com>
2022-03-14 22:35:32 -0500

    formatting in other way for test

--- pipeline/0x02-databases/17-store.sql
+++ pipeline/0x02-databases/17-store.sql
@@ -1,9 +1,8 @@
 -- script that creates a trigger that decreases the quantity of an item after adding a new order
 
-delimiter //
-
-CREATE TRIGGER update_items
-AFTER INSERT
+delimiter / / CREATE TRIGGER update_items
+AFTER
+INSERT
         ON orders FOR EACH ROW BEGIN
 update
         items
@@ -11,6 +10,5 @@ SET
         quantity = quantity - new.number
 WHERE
         items.name = new.item_name;
-END //
 
-delimiter;
+END / / delimiter;
