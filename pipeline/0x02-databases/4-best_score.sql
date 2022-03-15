1658bbb332d5acb4bebe9b76f5643e66db768948
“andresvanegas19” <andressantiagore@gmail.com>
2022-03-14 21:55:29 -0500

    adding the

---
+++ pipeline/0x02-databases/4-best_score.sql
@@ -0,0 +1,11 @@
+-- script that lists all records with a score >= 10 in the table second_table in your MySQL server.
+
+SELECT
+    score,
+    name
+FROM
+    second_table
+WHERE
+    score >= 10
+ORDER BY
+    score DESC;
