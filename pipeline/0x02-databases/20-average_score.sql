23bfdca2c66e10a6ceffeed30e2db94ff148285e
“andresvanegas19” <andressantiagore@gmail.com>
2022-03-14 22:46:48 -0500

    fixing the average putting diferent column

--- pipeline/0x02-databases/20-average_score.sql
+++ pipeline/0x02-databases/20-average_score.sql
@@ -1,21 +1,21 @@
 -- script that creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student.
 
-delimeter //
-CREATE PROCEDURE ComputeAverageScoreForUser(IN new_user_id INT) BEGIN
+delimiter //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id_new INTEGER) BEGIN
 UPDATE
         users
 SET
-        average_score = (
+        average_score =(
                 SELECT
                         AVG(score)
                 FROM
                         corrections
                 WHERE
-                        new_user_id = corrections.user_id
+                        user_id = user_id_new
         )
 WHERE
-        id = new_user_id;
+        id = user_id_new;
 
 END //
-delimeter;
+delimiter;
