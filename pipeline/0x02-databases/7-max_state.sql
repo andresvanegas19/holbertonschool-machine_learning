9fc9b9e2663d305364bdc58665a5d18ccf28990a
“andresvanegas19” <andressantiagore@gmail.com>
2022-03-14 22:04:37 -0500

    adding the temperature

---
+++ pipeline/0x02-databases/7-max_state.sql
@@ -0,0 +1,11 @@
+-- script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+
+SELECT
+    state,
+    MAX(value) AS max_temp
+FROM
+    temperatures
+GROUP BY
+    state
+ORDER BY
+    state ASC;
